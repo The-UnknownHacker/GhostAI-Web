3cbcfd0f07bea5134d017cdb9be1cd828943c71a
CyberZen <128781393+The-UnknownHacker@users.noreply.github.com>
2024-03-26 13:13:02 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Embedded Webpage</title>
+    </head>
+    <body>
+        <h1>Embedded Content</h1>
+        <iframe src="https://discord.com" width="100%" height="600px">
+            <p>Your browser does not support iframes.</p>
+        </iframe>
+    </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run(port=8080)
